0efa660c116420cdaa53815454b28cef60de98a6
Vasily Kirichenko <vaskir@gmail.com>
2017-04-24 22:07:03 +0300

    *

--- AkkaNetTest1/Script1.fsx
+++ AkkaNetTest1/Script1.fsx
@@ -1,12 +1,15 @@
-﻿#r @"../packages/Reactive.Streams/lib/net45/Reactive.Streams.dll"
-#r @"../packages/Akka/lib/net45/Akka.dll"
-#r @"../packages/Akka.Streams/lib/net45/Akka.Streams.dll"
+﻿#r @"../packages/Akka/lib/net45/Akka.dll"
+#r @"../packages/Hyperion/lib/net45/Hyperion.dll"
+#r @"../packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
 #r @"../packages/Akkling/lib/net45/Akkling.dll"
 #r @"../packages/Akka.Streams/lib/net45/Akka.Streams.dll"
+#r @"../packages/Akkling/lib/net45/Akkling.dll"
+#r @"../packages/Reactive.Streams/lib/net45/Reactive.Streams.dll"
+#r @"../packages/Akkling.Streams/lib/net45/Akkling.Streams.dll"
 
 open Akka.Streams
 open Akka.Streams.Dsl
-open Akka
+open Akka 
 open System.Numerics
 open Akka.IO
 open System.IO
@@ -18,15 +21,15 @@ Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 let system = System.create "system" (Configuration.defaultConfig())
 let mat = system.Materializer()
 
-let source: Source<int, NotUsed> = ofArray From [1..100]
+let source = Source.ofList [1..100]
 let factorials = source.Scan(BigInteger(1), fun acc next -> acc * BigInteger(next))
 
 let lineSink (fileName: string) =
     Flow.Create<string>()
-        .Select(fun num -> ByteString.FromString(sprintf "%O\n" num))
-        .ToMaterialized(FileIO.ToFile(FileInfo(fileName)), fun l r -> Keep.Right(l, r))
+    |> Flow.map (fun num -> ByteString.FromString(sprintf "%O\n" num))
+    |> Flow.toMat (FileIO.ToFile(FileInfo(fileName))) (fun l r -> Keep.Right(l, r))
 
 factorials
-    .ZipWith(Source.From [0..100], (fun num idx -> sprintf "%O! = %O" num idx))
-    .Throttle(1, TimeSpan.FromSeconds 1., 1, ThrottleMode.Shaping)
-    .RunForeach(Action<_> (printfn "%s"), mat)
+|> Source.zipWith (Source.ofList [0..100]) (fun num idx -> sprintf "%O! = %O" num idx)
+|> Source.throttle ThrottleMode.Shaping 1 1 (TimeSpan.FromSeconds 1.)
+|> Source.runForEach mat (printfn "%s")
